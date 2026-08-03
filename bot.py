d1f1428cd5c548c0f1029c56cd9de73ff00b3771
sujonmdsujonmdhgvvs-glitch <sujonmdsujonmdhgvvs@gmail.com>
2026-08-03 22:38:26 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Eshan Trading Bot Started")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "8965341019:AAEKz8Iz-5veDnJT8-saBTFj3rHUh6KC19k"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Eshan Trading Bot Started ✅")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+
+app.run_polling()
